270742effb211af9a391794465c700b105cdea24
Akash Khanra <akhanra@amazon.com>
2023-09-22 00:02:28 +0530

    Changed temp store to __dirname+'/public/uploads'

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(xss());
 
 const imageFileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, __dirname+'/uploads');
+        cb(null, __dirname+'/public/uploads');
     },
     filename: (req, file, cb) => {
         cb(null, new Date().toISOString() + '-' + file.originalname);
